bf547fe90b7abe2769f80c3c411e7084bf22c123
Ashish Yadav <199824394+ashish7802@users.noreply.github.com>
2026-03-14 18:24:22 +0530

    Create logger.py

---
+++ finance-tracker/backend/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+
+def setup_logger() -> logging.Logger:
+    logger = logging.getLogger("finance_tracker")
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    return logger
